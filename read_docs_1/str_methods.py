5e20a9cca2fa4327799b7aea24095b8a794c0eb1
Joel Turske <joelturske@gmail.com>
2024-11-19 20:05:50 -0500

    first few exercises that focused on str methods.

---
+++ read_docs_1/str_methods.py
@@ -0,0 +1,13 @@
+name = 'Benny Blanco'
+
+print(name.lower())
+
+print(name.rjust(25))
+
+reverse_name = list(name)
+reverse_name.reverse()
+print(''.join(reverse_name))
+
+reverse_name = name[::-1]
+print(reverse_name)
+
