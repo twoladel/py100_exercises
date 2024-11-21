f1ebc11d8d0163751bc8c7957f6b945682fe67ee
Joel Turske <joelturske@gmail.com>
2024-11-21 08:47:50 -0500

    cherry index exercise

---
+++ read_docs_1/lists.py
@@ -0,0 +1,7 @@
+fruits = ["apple", "banana", "cherry", "peach", "watermelon"]
+if 'cherry' in fruits:
+    cherry_index = fruits.index('cherry')
+    print(cherry_index)
+else:
+    print("Error, 'cherry' not in list! ")
+
