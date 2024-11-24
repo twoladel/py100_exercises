6bb6963512cf6d209cb9ad5b65542136bda34479
Joel Turske <joelturske@gmail.com>
2024-11-24 09:51:02 -0500

    weather 1 exercise, if, elif, else block.

---
+++ conditionals/weather.py
@@ -0,0 +1,8 @@
+weather = (input('How\'s the weather today? '))
+
+if weather == 'sunny':
+    print("It's a beautiful day!")
+elif weather == 'rainy':
+    print("Grab your umbrealla.")
+else:
+    print("Let's stay inside.")
