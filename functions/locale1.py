50805b3b261e9d2d0b3f7121ce8aa72b13e9a006
Joel Turske <joelturske@gmail.com>
2024-11-24 12:05:49 -0500

    extract the language code.

--- functions/locale1.py
+++ functions/locale1.py
@@ -5,3 +5,12 @@ def extract_language(locale):
 print(extract_language('en_US.UTF-8'))      # en
 print(extract_language('en_GB.UTF-8'))      # en
 print(extract_language('ko_KR.UTF-16'))     # ko
+
+# slice was 1st instinct, but I read ISO-639-3 is using 3 letter codes
+
+def extract_language1(locale):
+    return locale.split('_')[0]
+
+print(extract_language1('en_US.UTF-8'))
+print(extract_language1('en_GB.UTF-8'))
+print(extract_language1('ko_KR.UTF-16'))
