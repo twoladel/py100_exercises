b4079b9ad593dbe03ff1fd8ad960883f79d266ce
Joel Turske <joelturske@gmail.com>
2024-11-25 18:09:41 -0500

    determine if object is an instance of a type.

---
+++ lists/type.py
@@ -0,0 +1,9 @@
+some_value1 = [0, 1, 0, 0, 1]
+some_value2 = 'I leave you my Kingdom, take good care of it.'
+
+print(type(some_value1))
+print(type(some_value2))
+
+# 1st inclination was to check with type(). 
+print(isinstance(some_value1, list)) 
+print(isinstance(some_value2, list))
