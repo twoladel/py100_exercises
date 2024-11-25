901f42200b52d05395d2d71b7662b2f25868edb8
Joel Turske <joelturske@gmail.com>
2024-11-25 18:03:33 -0500

    print each element in a two dimensional list

---
+++ lists/vocab.py
@@ -0,0 +1,9 @@
+vocabulary = [
+    ['happy', 'cheerful', 'merry', 'glad'],
+    ['tired', 'sleepy', 'fatigued', 'drained'],
+    ['excited', 'eager', 'enthused', 'animated'],
+]
+
+for lst in vocabulary:
+    for word in lst:
+        print(word)
