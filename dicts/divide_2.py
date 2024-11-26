8a5a1d2521e515bd4213279a377cf772521fa30a
Joel Turske <joelturske@gmail.com>
2024-11-25 20:06:13 -0500

    added for loop because that is what exercise specified.

--- dicts/divide_2.py
+++ dicts/divide_2.py
@@ -4,5 +4,10 @@ numbers = {
     'low':    25,
 }
 
-half_numbers = [int(num / 2) for num in numbers.values()]
+half_numbers = [num // 2 for num in numbers.values()]
+print(half_numbers)
+
+half_numbers = []
+for num in numbers.values():
+    half_numbers.append(num // 2)
 print(half_numbers)
